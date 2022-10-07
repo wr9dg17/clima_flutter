65026a527346c2408f2b307af2471117d9dcf09d
Farid D. Hamzayev <Farid.Hamzayev@kapitalbank.az>
2022-10-07 19:41:58 +0400

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:clima_flutter/screens/loading_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(),
+      home: const LoadingScreen()
+    );
+  }
+}
+
